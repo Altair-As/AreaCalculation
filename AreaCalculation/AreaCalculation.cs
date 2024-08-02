6e4cba36cd8984e8184d307e9b0ac5aec8e0e79a
Altair_As <semalexandrov@gmail.com>
2024-08-02 21:23:16 +0300

    Add triangle area calculation

--- AreaCalculation/AreaCalculation.cs
+++ AreaCalculation/AreaCalculation.cs
@@ -4,9 +4,14 @@ namespace AreaCalculation
 {
     public class AreaCalculation
     {
-        public double AreaOfCircle(double radius)
+        public double Area(double radius)
         {
             return Circle.Area(radius);
+        }
+
+        public double Area(double a, double b, double c)
+        {
+            return Triangle.Area(a, b, c);
         }
     }
 }
--- AreaCalculation/Figures/Triangle.cs
+++ AreaCalculation/Figures/Triangle.cs
@@ -8,5 +8,32 @@ namespace AreaCalculation.Figures
 {
     internal static class Triangle
     {
+        // Formula: S = √(р (р — а)(р — b)(p — c))
+        // Or: S = (a * b) / 2
+
+        public static double Area(double a, double b, double c)
+        {
+            double[] sides = { a, b, c };
+            Array.Sort(sides);
+
+            double leg1 = sides[0];
+            double leg2 = sides[1];
+            double hypotenuse = sides[2];
+
+            if (IsRight(leg1, leg2, hypotenuse))
+            {
+                return (leg1 * leg2) / 2;
+            }
+            else
+            {
+                double p = (a + b + c) / 2;
+                return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            }
+        }
+
+        private static bool IsRight(double leg1, double leg2, double hypotenuse)
+        {
+            return Math.Pow(leg1, 2) + Math.Pow(leg2, 2) == Math.Pow(hypotenuse, 2);
+        }
     }
 }
