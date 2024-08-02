908f707b03c331796ee961ade29ed4ffac21f7ba
Altair_As <semalexandrov@gmail.com>
2024-08-02 21:34:10 +0300

    Add XML documentation

--- AreaCalculation/AreaCalculation.cs
+++ AreaCalculation/AreaCalculation.cs
@@ -4,11 +4,23 @@ namespace AreaCalculation
 {
     public class AreaCalculation
     {
+        /// <summary>
+        /// This method calculates the area of a circle.
+        /// </summary>
+        /// <param name="radius">The radius of the circe.</param>
+        /// <returns>The area of the circle.</returns>
         public double Area(double radius)
         {
             return Circle.Area(radius);
         }
 
+        /// <summary>
+        /// This method calculates the area of a triangle given the lengths of its three sides.
+        /// </summary>
+        /// <param name="a">The length of the first side of the triangle.</param>
+        /// <param name="b">The length of the second side of the triangle.</param>
+        /// <param name="c">The length of the third side of the triangle.</param>
+        /// <returns>The area of the triangle.</returns>
         public double Area(double a, double b, double c)
         {
             return Triangle.Area(a, b, c);
